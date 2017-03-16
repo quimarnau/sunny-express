6d25f5b7d05d003aeba8ac9e996e5a0c85cd7925
Matyas Tomolya <tomolya.matyas@gmail.com>
2017-03-16 22:28:17 +0000

    Implemented search cities external API service

--- app/js/testCtrl.js
+++ app/js/testCtrl.js
@@ -1,5 +1,28 @@
-sunnyExpressApp.controller('TestCtrl', function($scope, SunnyExpress) {
+sunnyExpressApp.controller('TestCtrl', function($scope, $q, SunnyExpress) {
 	
-	SunnyExpress.setDepartCity('Stockholm');
-	SunnyExpress.log(SunnyExpress.getDepartCity());
+	this.searchCity = function(numDays, city) {
+		var d = $q.defer();
+		var result = SunnyExpress.getCityWeather.get({days: numDays, q: city}, function() {
+			d.resolve(result);
+		});
+
+		return d.promise;
+	}
+
+	this.search = function() {
+		//TODO show loading picture
+		return SunnyExpress.responseParis;
+		var numDays = Math.round((SunnyExpress.getReturnDate()-SunnyExpress.getDepartDate())/(1000*60*60*24));
+		var cities = SunnyExpress.getActiveCities(SunnyExpress.getArriveCountry());
+		var cityQueue = [];
+		for (var i = 0; i < cities.length; i++) {
+			cityQueue.push(this.searchCity(numDays,cities[i]));
+		};
+		$q.all(cityQueue).then(function(data) {
+			//TODO hide loading picture
+		})
+	}
+
+	this.search();
+		
 });
