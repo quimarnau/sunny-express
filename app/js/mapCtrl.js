0215346d9c9021767a374cf1dbafd2b8351b0797
Matyas Tomolya <tomolya.matyas@gmail.com>
2017-03-18 14:36:06 +0000

    Implemented sights retrieval from google maps API

--- app/js/mapCtrl.js
+++ app/js/mapCtrl.js
@@ -1,21 +1,33 @@
-/**
- * Created by aleixsacrest on 17/03/2017.
- */
-
 var map;
 
 function initMap() {
-    alert('hholaa');
-    // Constructor creates a new map - only center and zoom are required.
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.7413549, lng: -73.9980244},
-        zoom: 13,
-        mapTypeControl: false
-    });
+	alert('hholaa');
+	var activeCity = {"name": "Paris", "lon":2.35236,"lat":48.856461};
+	var activeCityLocation = new google.maps.LatLng(activeCity.lat, activeCity.lon);
+	// Constructor creates a new map - only center and zoom are required.
+
+	var mapElement = document.getElementById('map');
+	map = new google.maps.Map(mapElement, {
+		center: activeCityLocation,
+		zoom: 13,
+		mapTypeControl: false
+	});
+
+	var request = {
+		location: activeCityLocation,
+		radius: '5000'
+	}
+
+	service = new google.maps.places.PlacesService(map);
+	service.nearbySearch(request, callback);
+
 }
 
+function callback(results, status) {
+	console.log(results);
+}
+
+
 sunnyExpressApp.controller('MapCtrl', function ($scope, SunnyExpress) {
-
-
 
 });
--- app/js/testCtrl.js
+++ app/js/testCtrl.js
@@ -1,6 +1,6 @@
 sunnyExpressApp.controller('TestCtrl', function($scope, $q, SunnyExpress) {
 	
-	this.searchCity = function(numDays, city) {
+	this.searchWeatherCity = function(numDays, city) {
 		var d = $q.defer();
 		var result = SunnyExpress.getCityWeather.get({days: numDays, q: city}, function() {
 			d.resolve(result);
@@ -9,16 +9,16 @@ sunnyExpressApp.controller('TestCtrl', function($scope, $q, SunnyExpress) {
 		return d.promise;
 	}
 
-	this.search = function() {
+	this.searchWeather = function() {
 		//TODO show loading picture
 		return SunnyExpress.responseParis;
 		
 		var numDays = Math.round((SunnyExpress.getReturnDate()-SunnyExpress.getDepartDate())/(1000*60*60*24));
-		var cities = SunnyExpress.getActiveCities(SunnyExpress.getArriveCountry());
+		var cities = SunnyExpress.getCountryCities(SunnyExpress.getArriveCountry());
 		var cityQueue = [];
 
 		for (var i = 0; i < cities.length; i++) {
-			cityQueue.push(this.searchCity(numDays,cities[i]));
+			cityQueue.push(this.searchWeatherCity(numDays,cities[i].name));
 		};
 
 		$q.all(cityQueue).then(function(data) {
@@ -26,6 +26,7 @@ sunnyExpressApp.controller('TestCtrl', function($scope, $q, SunnyExpress) {
 		})
 	}
 
-	this.search();
+
+	this.searchWeather();
 		
 });
