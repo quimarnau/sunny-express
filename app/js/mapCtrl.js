7bc6968da543a379d60d1599c92b601cb9264f8f
aleixsacrest <aleixsacrest@gmail.com>
2017-03-19 21:06:26 +0100

    integrate script index

--- app/js/mapCtrl.js
+++ app/js/mapCtrl.js
@@ -1,40 +1,36 @@
-var map;
-
-function initMap() {
-	var activeCity = {"name": "Paris", "lng":2.35236,"lat":48.856461};
-	var activeCityLocation = new google.maps.LatLng(activeCity.lat, activeCity.lng);
-	// Constructor creates a new map - only center and zoom are required.
-
-	var mapElement = document.getElementById('map');
-	map = new google.maps.Map(mapElement, {
-		center: activeCityLocation,
-		zoom: 12,
-		mapTypeControl: false
-	});
-	/*var marker = new google.maps.Marker({
-		map: map,
-        position: activeCityLocation,
-        title: 'Title of marker'
-    });//*/
-
-
-
-	var request = {
-		location: activeCityLocation,
-		radius: '5000'
-	}
-
-	//service = new google.maps.places.PlacesService(map);
-	//service.nearbySearch(request, callback);
-
-}
-
-function callback(results, status) {
-	console.log(results);
-}
-
-
 sunnyExpressApp.controller('MapCtrl', function ($scope, SunnyExpress) {
+
+	var map;
+
+    $(function () {
+        var activeCity = {"name": "Paris", "lng":2.35236,"lat":48.856461};
+        var activeCityLocation = new google.maps.LatLng(activeCity.lat, activeCity.lng);
+        // Constructor creates a new map - only center and zoom are required.
+
+        var mapElement = document.getElementById('map');
+        map = new google.maps.Map(mapElement, {
+            center: activeCityLocation,
+            zoom: 12,
+            mapTypeControl: false
+        });
+		/*var marker = new google.maps.Marker({
+		 map: map,
+		 position: activeCityLocation,
+		 title: 'Title of marker'
+		 });//*/
+
+        var request = {
+            location: activeCityLocation,
+            radius: '5000'
+        }
+
+        //service = new google.maps.places.PlacesService(map);
+        //service.nearbySearch(request, callback);
+    });
+
+    function callback(results, status) {
+        console.log(results);
+    }
 
 	$scope.refreshMap = function() {
 		SunnyExpress.setMapCenter(map);
