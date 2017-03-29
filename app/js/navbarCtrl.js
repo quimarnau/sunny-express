0b589e4f3f0991ed3f75267ced6d84e2aff46d84
Matyas Tomolya <tomolya.matyas@gmail.com>
2017-03-29 21:14:48 +0200

    Add description view to navbar

--- app/js/navbarCtrl.js
+++ app/js/navbarCtrl.js
@@ -2,7 +2,13 @@ sunnyExpressApp.controller('NavbarCtrl', function ($scope, $location, SunnyExpre
 
 	$scope.currentNavItem = $location.path();
 
+	$scope.selectedCity = SunnyExpress.getSelectedCity() != undefined ? SunnyExpress.getSelectedCity().toLowerCase() : undefined;
+	$scope.isDescription = $location.path().includes("description");
+
 	$scope.goTo = function (path) {
+		if(path.includes("description")) {
+			return;
+		}
   		$scope.currentNavItem = $location.path(path);
   	};
 });
