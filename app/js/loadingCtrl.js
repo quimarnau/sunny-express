74e09dc2c1b2f5da4d715b25d909547c089c830a
Matyas Tomolya <tomolya.matyas@gmail.com>
2017-04-01 20:11:06 +0100

    Small code fixes

--- app/js/loadingCtrl.js
+++ app/js/loadingCtrl.js
@@ -1,11 +1,9 @@
 sunnyExpressApp.controller('LoadingCtrl', function ($scope, $rootScope,$mdDialog, $interval, SunnyExpress) {
 
-	$scope.mode = 'query';
-
 	$rootScope.$on("loadingEvent",function(ev, isOn) {
 		if(isOn) {
 			$mdDialog.show({
-			  contentElement: '#myDialog',
+			  contentElement: '#loadingDialog',
 			  parent: angular.element(document.body),
 			  targetEvent: ev
 			});
