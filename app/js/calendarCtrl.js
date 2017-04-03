5d10284ea38b247cbf041da60c67a3d2bc9eade9
Laura <deleuze@kth.se>
2017-04-03 15:45:54 +0200

    Switch displaying/not displaying forecast in the calendar implemented and working

--- app/js/calendarCtrl.js
+++ app/js/calendarCtrl.js
@@ -40,30 +40,88 @@ sunnyExpressApp.controller('CalendarCtrl', function($scope, $filter, SunnyExpres
 			var departureText;
 			switch (trip.state) {
 				case 0:
-					departureText = "<p>Going to: <br><b>" + trip.data.arriveCity + "</b></p>"
+					departureText = "<p class=\"blue-event\">Going to: <br><b>" + trip.data.arriveCity + "</b></p>"
 					break;
 				case 1:
-					departureText = "<p>Coming back to: <br><b>" + trip.data.departCity + "</b></p>";
+					departureText = "<p class=\"blue-event\">Coming back to: <br><b>" + trip.data.departCity + "</b></p>";
 					break;
 				case 2:
-					departureText = "<p style=\"color:red\"><br>On a trip<br></p>";
+					departureText = "<p class=\"blue-event\">On a trip</p>";
 					break;
 			}
 			if (SunnyExpress.getForecastDisplay() == true) {
-				MaterialCalendarData.setDayContent(date, "<div align=\"center\">" + departureText + "<img src=\"../images/icons-map/sunny.png\"style=\"min-width: 30px; min-width: 30px;\"></div>");
+				MaterialCalendarData.setDayContent(date, "<div align=\"center\" layout:\"column\">" + departureText + "<img src=\"../images/icons-map/sunny.png\"style=\"min-width: 20px; min-width: 20px;\"></div>");
 			} else {
-				MaterialCalendarData.setDayContent(date, "<div align=\"center\">" + departureText + "</div>");
+				MaterialCalendarData.setDayContent(date, "<div align=\"center\" layout:\"column\">" + departureText + "</div>");
 			}
 		}
 	};
 
 	$scope.fillCalendar = fillCalendar;
+
+	var getDatesTrip = function(trip) {
+		var dates = [];
+		dates.push(trip.start);
+
+		date = new Date();
+		date.setDate(trip.start.getDate());
+
+
+		while (date.getTime() <= trip.end.getTime()) {
+			tmp = new Date();
+			tmp.setDate(date.getDate() + 1);
+			dates.push(tmp);
+			date = tmp;
+		}
+		return dates;
+	};
+
+	var getDateState = function(date, trip) {
+		if (date.toDateString() === trip.start.toDateString())
+			return {
+				"data": trip,
+				"state": 0
+			};
+		else if (date.toDateString() === trip.end.toDateString())
+			return {
+				"data": trip,
+				"state": 1
+			};
+		else
+			return {
+				"data": trip,
+				"state": 2
+			};		
+	};
 	
 
 	$scope.onChange = function(state) {
   		SunnyExpress.setForecastDisplay(!state);
-  		//$scope.fillCalendar = fillCalendar;  // -> find way to reload
-  		$scope.message = SunnyExpress.getForecastDisplay();
+  		var trips = SunnyExpress.getTrips();
+  		for (var i = 0; i < trips.length; i++) {
+  			var tripDates = getDatesTrip(trips[i]);
+  			for (j = 0; j < tripDates.length; j++) {
+  				var dateState = getDateState(tripDates[j], trips[i]);
+  				var departureText;
+				switch (dateState.state) {
+					case 0:
+						departureText = "<p class=\"blue-event\">Going to: <br><b>" + dateState.data.arriveCity + "</b></p>"
+						break;
+					case 1:
+						departureText = "<p class=\"blue-event\">Coming back to: <br><b>" + dateState.data.departCity + "</b></p>";
+						break;
+					case 2:
+						departureText = "<p class=\"blue-event\">On a trip</p>";
+						break;
+				}
+				if (SunnyExpress.getForecastDisplay() == true) {
+					MaterialCalendarData.setDayContent(tripDates[j], "<div align=\"center\" layout:\"column\">" + departureText + "<img src=\"../images/icons-map/sunny.png\"style=\"min-width: 20px; min-width: 20px;\"></div>");
+				} else {
+					MaterialCalendarData.setDayContent(tripDates[j], "<div align=\"center\" layout:\"column\">" + departureText + "</div>");
+				}
+  			}
+  			
+  		}
   	};
 
 });
