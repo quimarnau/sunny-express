e8bc6a83f24b8fd3314c857e6ba3c9de4eb5056a
Matyas Tomolya <tomolya.matyas@gmail.com>
2017-04-04 20:52:31 +0100

    Backend data made resolvable for every route

--- app/js/sunnyExpressService.js
+++ app/js/sunnyExpressService.js
@@ -29,13 +29,6 @@ sunnyExpressApp.factory("SunnyExpress", function ($resource, $filter, $timeout, 
 	var cities = undefined;
 	var countries = undefined;
 
-	/*var baseConditions = [1000, 1006, 1189, 1219]; // Sunny, Cloudy, Moderate rain, Moderate snow
-	var weatherConditionResolveDB = {
-									1000: [1000,1003],
-	 								1006: [1006,1009,1030,1135,1147],
-	 								1189: [1063,1087,1150,1153,1180,1183,1186,1189,1192,1195,1198,1201,1240,1243,1246,1273,1276],
-	 								1219: [1069,1072,1114,1117,1168,1171,1204,1207,1210,1213,1216,1219,1225,1237,1249,1252,1255,1258,1261,1264,1279,1282]
-	 								};*/
 	var baseConditions = undefined;
 	var weatherConditionResolveDB = undefined;
 
