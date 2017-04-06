a5b5a06713037f1f892892598bcef1e300fee089
Matyas Tomolya <tomolya.matyas@gmail.com>
2017-04-06 10:50:36 +0100

    Set up profile info retrieval after sign in

--- app/js/loginCtrl.js
+++ app/js/loginCtrl.js
@@ -1,7 +1,17 @@
 sunnyExpressApp.controller('LoginCtrl', function ($scope, SunnyExpress) {
 	$scope.$on('event:google-plus-signin-success', function (event, authResult) {
-		// Send login to server or save into cookie
-	});
+		gapi.client.load('plus', 'v1', apiClientLoaded);
+    });
+
+    function apiClientLoaded() {
+    	gapi.client.setApiKey("");
+        gapi.client.plus.people.get({userId: 'me'}).execute(handleResponse);
+    }
+
+    function handleResponse(resp) {
+        console.log(resp);
+    }
+
 	$scope.$on('event:google-plus-signin-failure', function (event, authResult) {
 		// Auth failure or signout detected
 	});
