d392c751b88d56205e326303ccc690c9542bf7e4
Laura <deleuze@kth.se>
2017-04-08 13:47:00 +0200

    Calendar - toolbar styling

--- app/js/calendarCtrl.js
+++ app/js/calendarCtrl.js
@@ -37,15 +37,22 @@ sunnyExpressApp.controller('CalendarCtrl', function($scope, $filter, $mdDialog, 
 
 	$scope.colorEvent = SunnyExpress.getColorEvent();
 
-		// //temporal trip for testing - to be removed afterwards
-		// var departDate = new Date();
-		// var returnDate = new Date();
-		// returnDate.setDate(departDate.getDate() + 2);
-		// var departCity = 'Stockholm';
-		// var returnCity = 'Madrid';
-		// var trip = {"start": departDate, "end": returnDate, "departCity": departCity, "arriveCity": returnCity};
-		// SunnyExpress.addNewTrip(trip);
-		// //end temporal trip
+	$scope.isOpen = false;
+
+      // $scope.demo = {
+      //   isOpen: false,
+
+      // };
+
+		//temporal trip for testing - to be removed afterwards
+		var departDate = new Date();
+		var returnDate = new Date();
+		returnDate.setDate(departDate.getDate() + 2);
+		var departCity = 'Stockholm';
+		var returnCity = 'Madrid';
+		var trip = {"start": departDate, "end": returnDate, "departCity": departCity, "arriveCity": returnCity};
+		SunnyExpress.addNewTrip(trip);
+		//end temporal trip
 
 	var setCalendarContent = function(date, trip) {
 		if (trip != null) {
